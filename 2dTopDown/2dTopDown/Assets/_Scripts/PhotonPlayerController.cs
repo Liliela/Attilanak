e7babef9299d803f84d224a2b2a3d06e606b7334
DESKTOP-V1TD94V\User <attila.kiss@leopoly.com>
2018-10-07 20:51:48 +0200

    fixes

--- 2dTopDown/2dTopDown/Assets/_Scripts/PhotonPlayerController.cs
+++ 2dTopDown/2dTopDown/Assets/_Scripts/PhotonPlayerController.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PhotonPlayerController : MonoBehaviour {
+public class PhotonPlayerController : Photon.MonoBehaviour
+{
+    public GameObject Camera;
 
     private void Awake()
     {
         PhotonNetworkManager.Instance.AddPlayer(this);
+        if (!photonView.isMine)
+        {
+            Camera.SetActive(false);
+        }
     }
     private void OnDestroy()
     {
